5b838af162d6eaa0677f7fef4506502226499066
bardincom <bardincom@icloud>
2020-02-24 17:27:44 +0200

    v. 0.1 Создан UITabBarController

--- Course2FinalTask/Course2FinalTask/Code/AppDelegate.swift
+++ Course2FinalTask/Course2FinalTask/Code/AppDelegate.swift
@@ -14,7 +14,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+      window = UIWindow(frame: UIScreen.main.bounds)
+      
+      let feedViewController = FeedViewController(nibName: nil, bundle: nil)
+      feedViewController.title = "Feed"
+      feedViewController.tabBarItem.image = UIImage(named: "feed")
+      let profileViewController = ProfileViewController(nibName: nil, bundle: nil)
+      profileViewController.title = "Profile"
+      profileViewController.tabBarItem.image = UIImage(named: "profile")
+      let tabBarController = UITabBarController()
+      tabBarController.tabBar.backgroundColor = .white
+      tabBarController.setViewControllers([feedViewController, profileViewController], animated: true)
+      
+      window?.rootViewController = tabBarController
+      
+      window?.makeKeyAndVisible()
         return true
     }
 }
---
+++ Course2FinalTask/Course2FinalTask/TabBarController/FeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FeedViewController.swift
+//  Course2FinalTask
+//
+//  Created by Aleksey Bardin on 24.02.2020.
+//  Copyright © 2020 Bardincom. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+
+}
